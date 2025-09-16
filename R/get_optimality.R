ac744ce9ad099c8603a79d7c6c376a9db27e727e
Tyler Morgan-Wall <tylermw@gmail.com>
2025-09-16 19:00:01 +0000

    version 1.9.0

--- R/get_optimality.R
+++ R/get_optimality.R
@@ -2,10 +2,10 @@
 #'
 #'@description Returns a list of optimality values (or one value in particular).
 #'
-#'Note: The choice of contrast will effect the `G` efficiency value, and `gen_design()`
-#'and `eval_design()` by default set different contrasts (`contr.simplex` vs `contr.sum`).
+#'Note: The choice of contrast will effect the `G` efficiency value, and [gen_design()]
+#'and [eval_design()] by default set different contrasts ([contr.simplex()] vs `contr.sum`).
 #'
-#'@param output The output of either gen_design or eval_design/eval_design_mc.
+#'@param output The output of either [gen_design()], [eval_design()], or [eval_design_mc()].
 #'@param optimality Default `NULL`. Return just the specific optimality requested.
 #'@param calc_g Default `FALSE`. Whether to calculate the g-efficiency.
 #'@return A dataframe of optimality conditions. `D`, `A`, and `G` are efficiencies (value is out of 100).
@@ -102,4 +102,18 @@ get_optimality = function(output, optimality = NULL, calc_g = FALSE) {
     return(optimality_df[optimality])
   }
   return(optimality_df)
+}
+
+#' @keywords internal
+calculate_i_optimality_safe = function(
+  model_matrix,
+  moment_matrix,
+  blockedVar
+) {
+  stopifnot(ncol(moment_matrix) == ncol(model_matrix))
+  IOptimality(
+    model_matrix,
+    moment_matrix,
+    blockedVar
+  )
 }
